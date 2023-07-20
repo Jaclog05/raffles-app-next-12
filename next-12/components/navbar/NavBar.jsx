950d3daf94b8b38872f04e7559a8306cd7654403
Jaider <jaidercueto.1010@gmail.com>
2023-07-20 18:43:31 -0500

    Auth0 login added. Login button changed to tag a

--- next-12/components/navbar/NavBar.jsx
+++ next-12/components/navbar/NavBar.jsx
@@ -9,7 +9,7 @@ export default function NavBar() {
       <ul>
         <Link href='#'><li>Jaider</li></Link>
         <Link href='/createRaffle'><li>Nueva Rifa</li></Link>
-        <Link href='/login'><li>Ingresar</li></Link>
+        <a href="/api/auth/login">Login</a>
       </ul>
     </header>
   )
--- next-12/pages/_app.js
+++ next-12/pages/_app.js
@@ -1,13 +1,16 @@
 import '../styles/globals.css'
 import NavBar from '../components/navbar/NavBar'
 import { ThemeProvider } from '../context/raffleState'
+import { UserProvider } from '@auth0/nextjs-auth0/client';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ThemeProvider >
-      <NavBar/>
-      <Component {...pageProps} />
-    </ThemeProvider> 
+    <UserProvider>
+      <ThemeProvider >
+        <NavBar/>
+        <Component {...pageProps} />
+      </ThemeProvider> 
+    </UserProvider>
   )
 }
 
