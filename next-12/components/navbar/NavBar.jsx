63c82f533a7be71a77a3789b3d659b751a47645a
Jaider <jaidercueto.1010@gmail.com>
2023-07-21 17:10:27 -0500

    Adding name in nav after login. Styles

--- next-12/components/navbar/NavBar.jsx
+++ next-12/components/navbar/NavBar.jsx
@@ -1,16 +1,27 @@
 import React from 'react'
 import Link from 'next/link'
 import styles from './NavBar.module.css'
+import { useUser } from '@auth0/nextjs-auth0/client';
 
 export default function NavBar() {
+
+  const { user } = useUser();
+
   return (
     <header className={styles.container}>
       <Link href='/'><div>Raffles App</div></Link>
-      <ul>
-        <Link href='#'><li>Jaider</li></Link>
-        <Link href='/createRaffle'><li>Nueva Rifa</li></Link>
-        <a href="/api/auth/login">Login</a>
-      </ul>
+      {
+        user ? 
+        <ul>
+            <Link href='#'><li>{user.nickname}</li></Link>
+            <Link href='/createRaffle'><li>Nueva Rifa</li></Link>
+            <a className={styles.login} href="/api/auth/logout">Cerrar Sesion</a>
+        </ul>
+        :
+        <ul>
+            <a className={styles.login} href="/api/auth/login">Ingresar</a>
+        </ul>
+      }
     </header>
   )
 }
