3e84283abbd0a93c7b1f1eeb228c3998220166e5
Jaider <jaidercueto.1010@gmail.com>
2023-07-22 11:18:00 -0500

    initialState_user.js file created for cleaning form after submit

---
+++ next-12/pages/register/initialState_user.js
@@ -0,0 +1,10 @@
+export const initialState_user = {
+    name: '',
+    email: '',
+    password: '',
+    confirmPassword: '',
+    cellPhone: '',
+    address: '',
+    idType: 1,
+    idNum: ''
+}
