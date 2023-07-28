94b376b962cfc250d984e5178c5dbccecde8b53d
Jaider <jaidercueto.1010@gmail.com>
2023-07-28 18:53:30 -0500

    using express.json instead on body-parser. setting corsOptions middleware

--- api/src/server.js
+++ api/src/server.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const bodyParser = require('body-parser')
+const cookies = require('cookie-parser')
 const cors = require('cors')
 const morgan = require('morgan')
 
@@ -13,10 +13,20 @@ app.set('port', process.env.PORT || 4000)
 
 //MiddleWares
 
+app.use(express.json())
 app.use(express.urlencoded({extended: false}))
-app.use(bodyParser.json({ limit: '50mb' }))
+app.use(cookies())
 app.use(morgan('dev'))
-app.use(cors())
+
+const corsOptions = {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+};
+
+
+app.use(cors(corsOptions))
 
 //routes
 
