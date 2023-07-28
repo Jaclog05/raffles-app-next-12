31a25d518f59ada37cf20c68d82761d4509cdf0c
Jaider <jaidercueto.1010@gmail.com>
2023-07-28 18:59:31 -0500

    Creating user local state on layout. sending it to navBar and login page

--- next-12/pages/_app.js
+++ next-12/pages/_app.js
@@ -1,15 +1,37 @@
 import '../styles/globals.css'
 import NavBar from '../components/navbar/NavBar'
 import { ThemeProvider } from '../context/raffleState'
+import axios from 'axios';
+import { useState, useEffect } from 'react';
 
-function MyApp({ 
-  Component, 
-  pageProps: { session, ...pageProps } 
-}) {
+function MyApp({ Component, pageProps }) {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    checkLoginStatus();
+  }, []);
+
+  const checkLoginStatus = async () => {
+
+    try {
+      const response = await axios.get("http://localhost:4000/profile", {
+        withCredentials: true
+      });
+      const data = response.data;
+
+      if (data.hasOwnProperty('email') && data.email.length) {
+        setUser(data);
+      } else {
+        setUser(null);
+      }
+    } catch (error) {
+      setUser(null);
+    }
+  };
   return (
       <ThemeProvider >
-        <NavBar/>
-        <Component {...pageProps} />
+        <NavBar user={user} checkStatus={checkLoginStatus}/>
+        <Component changeStatus={checkLoginStatus} {...pageProps} />
       </ThemeProvider>
   )
 }
