367c6e3a065a9a649f812b2fc8c5b889d704d5b8
Jaider <jaidercueto.1010@gmail.com>
2023-07-31 14:00:21 -0500

    protecting dashboard routes and subroutes

--- next-12/middleware.js
+++ next-12/middleware.js
@@ -1,9 +1,21 @@
 import { NextResponse } from "next/server";
+import {jwtVerify} from "jose"
 
-export function middleware() {
+export async function middleware(request) {
+
+  const jwt = request.cookies.get('token')
   
-  console.log('middleWare')
-  return NextResponse.next();
+  if(!jwt){
+    return NextResponse.redirect(new URL('/login', request.url))
+  }
+
+  try {
+    const {payload} = await jwtVerify(jwt.value, new TextEncoder().encode('secret'))
+    return NextResponse.next()
+  } catch (error) {
+    return NextResponse.redirect(new URL('/login', request.url))
+  }
+
 }
 
 export const config = {
